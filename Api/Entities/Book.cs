1fb0791145c35f0d67229dd6a2fe948f91039059
Boris Korobeinikov <slon2k@gmail.com>
2020-07-30 10:00:47 +0600

    Add project files.

---
+++ Api/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Entities
+{
+    public class Book : Entity
+    {         
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Author Author { get; set; }
+
+        public int AuthorId { get; set; }
+    }
+}
